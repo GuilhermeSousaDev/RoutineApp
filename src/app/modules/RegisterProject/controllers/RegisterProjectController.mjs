209ad8c5b7c3bce7599eb97fa9820afe10f9107d
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-07-11 20:42:49 -0300

    using api createProject method

--- src/app/modules/RegisterProject/controllers/RegisterProjectController.mjs
+++ src/app/modules/RegisterProject/controllers/RegisterProjectController.mjs
@@ -1,12 +1,17 @@
 export default class RegisterProjectController {
-    constructor({ effects, render }) {
+    constructor({ effects, render, api }) {
         this.effects = effects;
         this.render = render;
+        this.api = api;
 
         this.btnAdd = document.querySelector('.btn-add');
         this.btnNextStep = document.querySelector('.btn-shadow');
         this.registerProjectArea = document.querySelector('.register-project-area');
-        this.registerNameInput = document.querySelector('.register-name-input');
+
+        this.registerNameInput = 
+            document.querySelector('.register-project-container').children[1].children[0];
+        this.registerDetailsInput = 
+            document.querySelector('.register-project-container').children[2].children[0];
     }
 
     showRegisterProjectArea() {
@@ -18,9 +23,16 @@ export default class RegisterProjectController {
         );
     }
 
-    registerProject() {
+    async registerProject() {
         this.btnNextStep.addEventListener('click', () => {
-            console.log('create');
+            const name = this.registerNameInput.value;
+            const details = this.registerDetailsInput.value;
+
+            if (name.length && details.length) {
+                const project = await this.api.createProject({ name, details });
+
+                console.log(project);
+            }
         });
     }
 }
--- src/app/modules/RegisterProject/index.mjs
+++ src/app/modules/RegisterProject/index.mjs
@@ -2,15 +2,18 @@ import RegisterProjectController from "./controllers/RegisterProjectController.m
 
 import Effects from "./methods/Effects.mjs";
 import Render from "./methods/Render.mjs";
+import Api from "./methods/Api.mjs";
 
 export default class RegisterProject {
     constructor() {
         const effects = new Effects();
         const render = new Render();
+        const api = new Api();
 
         return new RegisterProjectController({ 
             effects,
-            render
+            render,
+            api
         });
     }
 }

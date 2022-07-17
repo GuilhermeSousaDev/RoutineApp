0c4d1c21405e82c88fc204b85c30c751043d6ded
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-07-17 11:45:47 -0300

    Resolving problem cors

--- src/app/index.mjs
+++ src/app/index.mjs
@@ -5,6 +5,8 @@ import View from './modules/View/index.mjs';
 import RegisterProject from './modules/RegisterProject/index.mjs';
 import Profile from './modules/Profile/index.mjs';
 
+io('http://localhost:8081');
+
 const view = new View();
 const registerProject = new RegisterProject();
 const profile = new Profile();

b3c2accbfdaf9de9e50ebbb60b1c74ba6a73706c
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-07-30 18:50:22 -0300

    Set Token in LocalStorage

--- src/app/modules/Auth/methods/AuthMethods.mjs
+++ src/app/modules/Auth/methods/AuthMethods.mjs
@@ -3,19 +3,22 @@ import { apiConfig } from '../../../config/apiConfig.mjs';
 export default class AuthMethods {
     async handleAuthenticate({ email, password }) {
         if (email.length && password.length) {
-            const form = new FormData();
-
-            form.append('email', email);
-            form.append('password', password);
+            const form = {
+                email: email,
+                password: password
+            }
 
             const request = await fetch(`${apiConfig.url}/session`, {
                 method: "POST",
-                body: form,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form),
             });
 
             const response = await request.json();
 
-            console.log(response);
+            localStorage.setItem('token', response.token);
         }
     }
 }
